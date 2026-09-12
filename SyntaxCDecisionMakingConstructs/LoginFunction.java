60d1da0d90140daf9d0dc6824d4b2d97d439b92c
Tae Nguyen <nguyenductay121999@gmail.com>
2026-09-12 20:28:58 +0700

    12/09/26

---
+++ SyntaxCDecisionMakingConstructs/LoginFunction.java
@@ -0,0 +1,28 @@
+package SyntaxCDecisionMakingConstructs;
+
+import java.util.Scanner;
+
+public class LoginFunction {
+    public static void main(String[] args) {
+        String userName = "root";
+        String password = "admin";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String inputUsername = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String inputPassword = scanner.nextLine();
+
+        scanner.close();
+
+        if (inputUsername.equals(userName) && inputPassword.equals(password)) {
+            System.out.println("Login successful!");
+        } else if (inputUsername.equals(userName)) {
+            System.out.println("Invalid password.");
+        } else {
+            System.out.println("User not found.");
+        }
+    }
+}
